c06bef75cca521ac4bbce4f3f7e4db951d8dc61b
Kyle Reneg Aduan <kylereneg1234@gmail.com>
2025-09-07 15:27:21 +0800

    Remove build directory from tracking

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_OpenTournament_FILE I:/VS_Code_Programs/Portfolio/Desktop-Applications/OpenTournament/build/OpenTournament.exe)
-set(__QT_DEPLOY_TARGET_OpenTournament_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_OpenTournament_RUNTIME_DLLS H:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;H:/Qt/6.9.2/mingw_64/bin/Qt6Sql.dll;H:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;H:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
